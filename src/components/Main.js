7a34ac9c422b199a3e11032a1790df35fe0ad07e
salmanzaheer1 <salmanzaheer1@gmail.com>
2023-04-14 00:08:25 +1000

    Added JSON to represent data & made components dynamic

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HotelInfo from './HotelInfo.js';
+import Welcome from './Welcome.js';
+const Main=()=>{
+return(
+
+  <main id="wrapper">
+  <Welcome />
+
+<HotelInfo />
+</main>
+
+
+);
+}
+
+export default Main;
